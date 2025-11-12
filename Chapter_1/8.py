ef8d094d073313f48abe284f03f9cb86bc3107e7
Rohan Pathak <rohanpathak2106@gmail.com>
2025-11-12 18:07:53 +0530

    start chapter 1

--- Chapter_1/8.py
+++
@@ -1,9 +0,0 @@
-"""
-Assignment
-Fantasy Quest has a bug in its server code. The code is supposed to print messages indicating the game server has started successfully.
-
-Fix both syntax errors in the code.
-"""
-
-print("Starting up game server...")
-print("local game server is listening on port 8080")
