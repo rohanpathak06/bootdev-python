9fc92fe2010aa384e93e7766e55c92e1df88381e
Rohan Pathak <rohanpathak2106@gmail.com>
2025-11-22 11:30:13 +0530

    complete chapter 2

---
+++ CHAPTER_2/5.py
@@ -0,0 +1,10 @@
+"""
+Assignment
+Line #1 in the code was meant to be a comment, but the developer forgot to use the correct syntax (#).
+
+Fix the bug by commenting out the first line of code.
+"""
+
+# the best_sword variable holds the value of the best sword in the game
+best_sword = "scimitar"
+print(best_sword)
---
+++ CHAPTER_2/9.py
@@ -0,0 +1,19 @@
+"""Assignment
+We have a second player in our game!
+
+We need to tell each of our players how much health they have left.
+
+Edit line 9 to print Player 1's health: You have 1200 health using string concatenation and the variables provided
+Edit line 10 to print Player 2's health: You have 1100 health in the same way"""
+
+sentence_start = "You have "
+sentence_end = " health"
+
+player1_health = "1200"
+player2_health = "1100"
+
+# Don't edit above this line
+
+print(sentence_start + player1_health + sentence_end)
+print(sentence_start + player2_health + sentence_end)
+
