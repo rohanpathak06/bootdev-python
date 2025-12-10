afa6f74da0379ffdf91eb9b0ad9ee914892b05c3
Rohan Pathak <rohanpathak2106@gmail.com>
2025-12-10 10:55:14 +0530

    first lesson

--- Chapter_1/3.py
+++
@@ -1,8 +0,0 @@
-"""
-Assignment
-We need to greet our new players! Print the following text to the console:
-
-Greetings, adventurer!
-"""
-
-print("Greetings, adventurer!")
--- Chapter_1/6.py
+++
@@ -1,7 +0,0 @@
-"""
-assignment
-The code was supposed to print Welcome to Fantasy Quest! to our users, 
-but they get an error instead. Find the syntax error in the code editor, and fix it.
-"""
-
-print("Welcome to Fantasy Quest!")
